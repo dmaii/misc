b1015e77cfbfad32906ae4ba107f26bec863d413
D M <gs9600@gmail.com>
2014-01-26 19:22:51 -0500

    first commit

---
+++ src/test/java/euler/ProjectEulerTest.java
@@ -0,0 +1,14 @@
+package euler;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ProjectEulerTest {
+	
+	
+	@Test
+	public void testP1(){
+		assertEquals(233168, new P1().run());
+	}
+}
